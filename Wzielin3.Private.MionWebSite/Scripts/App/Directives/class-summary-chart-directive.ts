93db4e265ff301ccc0b68dc633b5818b519405d0
Wojciech Zieliński <zielinski.w.t@gmail.com>
2015-07-07 08:56:21 +0000

    First chart is displaying but with data from wrong ng-repeat (the last one). Needs fix.

--- Wzielin3.Private.MionWebSite/Scripts/App/Directives/class-summary-chart-directive.ts
+++ Wzielin3.Private.MionWebSite/Scripts/App/Directives/class-summary-chart-directive.ts
@@ -1,19 +1,33 @@
 ﻿module App.Directives {
     "use strict";
     export class ClassSummaryChartDirective implements ng.IDirective {
-        public link: (scope: Scopes.IClassSummaryChartScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => void;
         public templateUrl = "../../../Views/ClassSummaryChart.html";
         public restrict = "E";
-        public scope: {
-            classes: string;
+        public scope = {
+            classes: "="
         };
-
-        constructor() {
-            this.scope = { classes: "=" };
-            ClassSummaryChartDirective.prototype.link =
-            (scope: Scopes.IClassSummaryChartScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
-                scope.name = "Aaron";
-            };
+        public link =
+        (scope: Scopes.IClassSummaryChartScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
+        };
+        public controller = ($scope: Scopes.IClassSummaryChartScope) => {
+            $scope.chartData = new Services.Model.ClassSummaryChartModel();
+            $scope.chartData.data = [{
+                key: "Grades summary",
+                values: []
+            }];
+            var values = [];
+            for (var i = 0; i < $scope.classes.length; ++i) {
+                var currentGrade = $scope.classes[i].grade;
+                if (values[currentGrade] === undefined) {
+                    values[currentGrade] = 0;
+                }
+                values[currentGrade]++;
+            }
+            for (var property in values) {
+                if (values.hasOwnProperty(property)) {
+                    $scope.chartData.data[0].values.push([property, values[property]]);
+                }
+            }
         }
 
         public static Factory() {
@@ -29,6 +43,16 @@
 module App.Directives.Scopes {
     "use strict";
     export interface IClassSummaryChartScope extends ng.IScope {
-        name: string;
+        classes: App.Services.Model.Classes[];
+        chartData: Services.Model.ClassSummaryChartModel;
     }
 } 
+
+module App.Services.Model {
+    export class ClassSummaryChartModel {
+        data: {
+            key: string;
+            values: any[];
+        }[];
+    }
+}
