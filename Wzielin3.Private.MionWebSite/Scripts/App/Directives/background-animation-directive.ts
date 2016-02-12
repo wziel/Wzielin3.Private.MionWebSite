203779ef8b2b7a67653687e21796013e4e1a217e
Wojciech Zieliński <zielinskiwt@outlook.com>
2016-02-12 14:08:34 +0100

    Calculating density of generated background elements based on window height, average font, movement speed.

--- Wzielin3.Private.MionWebSite/Scripts/App/Directives/background-animation-directive.ts
+++ Wzielin3.Private.MionWebSite/Scripts/App/Directives/background-animation-directive.ts
@@ -3,33 +3,57 @@
     export class BackgroundAnimationDirective implements ng.IDirective {
         public templateUrl = "Views/BackgroundAnimation.html";
         public restrict = "E";
-        public scope = { };
+        public scope = {};
         public link = (scope: Scopes.IBackgroundAnimationScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => { };
         public controller = ($scope: Scopes.IBackgroundAnimationScope) => {
             var texts = [".NET", "C#", "SharePoint", "JavaScript", "TypeScript", "HTML", "CSS", "AngularJS", "ASP.NET",
                 "Visual Studio", "GIT", "TFS", "Windows", "MVC"]
             var animateContainer = document.getElementById("background-animation-container");
+            var avrgFontSize = 70;
+            var avrgTextMoveAwayTime = calculateAvrgTextMoveAwayTime();
+            var animationLengthMs = 60000;
             var drawElement = function () {
                 var animateElement = document.createElement("span");
                 animateElement.classList.add("background-animation-element-look");
                 animateElement.innerHTML = texts[Math.floor(Math.random() * texts.length)];
-                animateElement.style.opacity = (Math.random() * 0.4 + 0.2).toString();
-                animateElement.style.fontSize = (Math.random() * 60 + 40).toString() + "px";
+                animateElement.style.opacity = (Math.random() * 0.4 + 0.3).toString();
+                animateElement.style.fontSize = (Math.random() * 60 - 30 + avrgFontSize).toString() + "px";
                 animateContainer.appendChild(animateElement);
 
-                animateElement.style.top = (Math.random() * (window.innerHeight - animateElement.offsetHeight - 52)+ 52).toString() + "px";
-                animateElement.style.left = (-animateElement.offsetWidth - Math.random()*100).toString() + "px";
+                animateElement.style.top = (Math.random() * (window.innerHeight - animateElement.offsetHeight)).toString() + "px";
+                animateElement.style.left = (-animateElement.offsetWidth).toString() + "px";
                 animateElement.style.width = (animateElement.offsetWidth + 10).toString() + "px";
 
                 animateElement.classList.add("background-animation-element-animate");
 
-                setTimeout(drawElement, 2000);
-                setTimeout(removeElement, 60000, animateElement);
-            };
-            var removeElement = function (element) {
-                animateContainer.removeChild(element);
+                var nextElementIn = 2 * avrgTextMoveAwayTime * avrgFontSize / window.innerHeight;
+                setTimeout(drawElement, nextElementIn);
+                setTimeout(removeElement, animationLengthMs, animateElement);
             };
             drawElement();
+
+            function removeElement(element) {
+                animateContainer.removeChild(element);
+            };
+
+            ///calculate the average time in which texts moves it's whole length
+            function calculateAvrgTextMoveAwayTime(): number {
+                var sumLength = 0;
+                var element = document.createElement("span");
+                element.classList.add("background-animation-element-look");
+                element.style.fontSize = avrgFontSize.toString() + "px";
+                element.style.opacity = "0";
+                document.body.appendChild(element);
+                for (var i = 0; i < texts.length; ++i) {
+                    element.innerHTML = texts[i];
+                    sumLength += element.offsetWidth;
+                }
+                document.body.removeChild(element);
+                var avrgLength = sumLength / texts.length;
+                var avrgSpeed = (2000 + avrgLength) / 60000;
+                var avrgTime = avrgLength / avrgSpeed;
+                return avrgTime;
+            }
         };
 
         public static Factory() {
